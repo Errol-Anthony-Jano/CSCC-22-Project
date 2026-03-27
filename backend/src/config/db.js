8537c82d19b2487ed2e6ef895d81abb824a4c598
Errol Anthony Jano <200910884@my.xu.edu.ph>
2026-03-27 16:07:51 +0800

    Create connection to db

---
+++ backend/src/config/db.js
@@ -0,0 +1,22 @@
+import { Sequelize } from 'sequelize';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+import dotenv from 'dotenv';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+dotenv.config({ path: resolve(__dirname, '../../.env') });
+
+const DB_URL = `postgres://${process.env.PG_USER}:${process.env.PG_PASSWORD}@db:5432/${process.env.PG_DB}`
+
+export const sequelize = new Sequelize(DB_URL)
+
+export const testDBConnection = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
