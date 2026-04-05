1bd4d4016b701b0f80e36247c5248c90e5b563f1
Errol Anthony Jano <200910884@my.xu.edu.ph>
2026-04-05 15:12:58 +0800

    Remove umzug.up() function call

--- backend/src/config/migrator.js
+++ backend/src/config/migrator.js
@@ -4,13 +4,11 @@ import { Umzug, SequelizeStorage } from "umzug";
 import { sequelize } from "./db.js";
 
 const umzug = new Umzug({
-  migrations: { glob: 'migrations/*.js' },
+  migrations: { glob: 'src/migrations/*.js' },
   context: sequelize.getQueryInterface(),
   storage: new SequelizeStorage({ sequelize }),
   logger: console,
 })
-
-await umzug.up();
 
 if (import.meta.url === `file://${process.argv[1]}`) {
   umzug.runAsCLI();
