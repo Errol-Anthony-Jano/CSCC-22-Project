ed6c7abb96742e5c8e159a95dd40d676ab825814
Errol Anthony Jano <200910884@my.xu.edu.ph>
2026-04-13 11:49:22 +0800

    Refactor products route to isolate logic

---
+++ backend/src/controllers/productsController.js
@@ -0,0 +1,43 @@
+import models from "../config/db.js";
+
+export const insertNewProduct = async (req, res) => {
+    try {
+        await models.products.create(req.body);
+        res.json({
+            message: "Product added successfully.",
+            data: req.body,
+        })
+    }
+    catch (err) {
+        res.status(400).json({
+            message: "Error adding product.",
+            error: err.message,
+        })
+    }
+}
+
+export const updateExistingProduct = async (req, res) => {
+    try {
+        const productId = req.params.productId;
+        const product = await models.products.findByPk(productId);
+
+        if (!product) {
+            return res.status(404).json({ message: `Product ${productId} not found.` })
+        }
+        
+        await models.products.update(req.body, {
+            where: {
+                product_id: productId,
+            }
+        })
+        
+        return res.json({
+            message: "Product updated successfully.",
+            data: req.body,  
+        })
+        
+    }
+    catch (error) {
+        return res.status(500).json({ message: "Error updating product."});
+    }
+}
--- backend/src/routes/products.js
+++ backend/src/routes/products.js
@@ -3,6 +3,7 @@ export const productsRouter = express.Router();
 import models from "../config/db.js";
 import { validateInsertPayload } from "../middleware/productsMiddleware.js";
 import { validatePatchPayload } from "../middleware/productsMiddleware.js";
+import { insertNewProduct, updateExistingProduct } from "../controllers/productsController.js";
 
 const productsSchema = {
   product_name: "string",
@@ -19,39 +20,7 @@ productsRouter.get('/', async (req, res) => {
 })
 
 // -> add new product
-productsRouter.post('/', validateInsertPayload(productsSchema), async (req, res) => {
-  try {
-    await models.products.create(req.body);
-    res.json({
-      message: "Product added successfully.",
-      data: req.body,
-    })
-  }
-  catch (err) {
-    res.status(400).json({
-      message: "Error adding product.",
-      error: err.message,
-    })
-  }
-})
+productsRouter.post('/', validateInsertPayload(productsSchema), insertNewProduct)
 
 // update any of the product details (except for product_id)
-productsRouter.patch('/:productId', validatePatchPayload(productsSchema), async (req, res) => {
-  try {
-    const productId = req.params.productId;
-    await models.products.update(req.body, {
-      where: {
-        product_id: productId,
-      }
-    })
-    res.json({
-      message: "Product updated successfully.",
-      data: req.body,
-    })
-  } catch (err) {
-    res.status(400).json({
-      message: "Error updating product.",
-      error: err.message,
-    })
-  }
-})
+productsRouter.patch('/:productId', validatePatchPayload(productsSchema), updateExistingProduct);
