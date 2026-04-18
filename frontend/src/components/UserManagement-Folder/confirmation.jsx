c73a86a0beba4b5ad8a1774f6e52a8b2e0914e74
Keisha Layne Delgado <20230027801@my.xu.edu.ph>
2026-04-18 21:52:34 +0800

    Fix confirmation popup -added OnConfirm

--- frontend/src/components/UserManagement-Folder/confirmation.jsx
+++ frontend/src/components/UserManagement-Folder/confirmation.jsx
@@ -1,12 +1,12 @@
 import styles from "./confirmation.module.css";
 
-function Confirmation({onClose}) {
+function Confirmation({onConfirm, onClose}) {
     return (
         <div className={styles.confirmationpage}>
             <div className={styles.confirmationcontainer}>
                 <h1 className={styles.confirmationtitle}>REMOVING</h1>
                 <p className={styles.confirmationmessage}>Are you sure you want to remove this user?</p>
-                <button onClick={onClose} className={styles.confirmbuttons}>Yes</button> {/*need to change this if hi connect sa database*/}
+                <button onClick={onConfirm} className={styles.confirmbuttons}>Yes</button> {/*need to change this if hi connect sa database*/}
                 <button onClick={onClose} className={styles.confirmbuttons}>Cancel</button>
             </div>
         </div>
