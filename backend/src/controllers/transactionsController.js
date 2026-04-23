f1f2fcebf6682d9de429c8641e5072b2c6210695
Errol Anthony Jano <200910884@my.xu.edu.ph>
2026-04-24 01:22:10 +0800

    Refactor clunky update function to prevent race conditions

--- backend/src/controllers/transactionsController.js
+++ backend/src/controllers/transactionsController.js
@@ -2,7 +2,6 @@ import { registerConsoleShortcuts } from "vitest/node";
 import models, { sequelize } from "../config/db.js";
 import { where } from "sequelize";
 import { getValidProduct, getValidUser, insertTransactionItem, isPayloadIdentical, patchTransactionPayload, validatePurchasedAmount } from "../services/transactionService.js";
-import _ from "lodash";
 // TODO: Create custom object comparison function for old and new payloads
 
 export const insertTransaction = async (data, t) => {
@@ -16,47 +15,37 @@ export const insertTransaction = async (data, t) => {
         const product = await getValidProduct(item.product_id, t);
         const productJSON = product.toJSON();
         validatePurchasedAmount(item.quantity_bought, product.product_quantity)   
-        const txnItem = await insertTransactionItem(productJSON, txnJSON, item, t)  
-        await product.decrement('product_quantity', { by: item.quantity_bought }, { transaction: t });
+        const txnItem = await insertTransactionItem(productJSON, txnJSON, item, t);
+
+        await models.products.decrement('product_quantity', {
+            by: item.quantity_bought,
+            where: {
+                product_id: item.product_id,
+            },
+            transaction: t,
+        })
     }
 
-    return txnJSON;
+    return txn;
 }
 
-export const updateTransaction = async (transactionId, patchPayload, t) => {
-    const txn = await models.transactions.findByPk(transactionId, {
-        include: {
-            model: models.transaction_items,
-            as: "transaction_items",
-        }
-    });
-    const txnJSON = txn.toJSON();
-
-    const updatedPayload = patchTransactionPayload(txnJSON, patchPayload);
-
-    isPayloadIdentical(txnJSON, updatedPayload);
-
-    updatedPayload["prev_txn_id"] = txn.transaction_id;
-    
+export const updateTransaction = async (oldTxn, updatedPayload, t) => {
+    console.log("im here");
     // rollback items
-    for (const item of txnJSON.transaction_items) {
+    for (const item of oldTxn.transaction_items) {
         const product = await getValidProduct(item.product_id, t);
-        await product.increment('product_quantity', { by: item.quantity_bought }, { transaction: t });
-    }
-    
-    // remove fields first
-    delete updatedPayload["transaction_id"];
-
-    for (const item of updatedPayload.transaction_items) {
-        delete item["transaction_id"];
-        delete item["item_id"];
-        delete item["product_name"];
-        delete item["product_unit_price"];
+        await product.increment('product_quantity', {
+            by: item.quantity_bought,
+            transaction: t,
+        })
     }
 
-    await txn.update({ voided_at: Date.now () }, { transaction: t })
+    await models.transactions.update(
+        { voided_at: new Date(), prev_txn_id: oldTxn.transaction_id },
+        { where: { transaction_id: oldTxn.transaction_id }, transaction: t }
+    );
 
-    const result = await insertTransaction(updatedPayload, t);
+    const newTxn = await insertTransaction(updatedPayload, t);
 
-    return result;
+    return newTxn;
 }
