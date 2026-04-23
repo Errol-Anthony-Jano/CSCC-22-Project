d01ca6b110d6f61338e33e156519b287ee0ff61d
Errol Anthony Jano <200910884@my.xu.edu.ph>
2026-04-24 01:22:32 +0800

    Update middleware suite

--- backend/src/middleware/transactionsMiddleware.js
+++ backend/src/middleware/transactionsMiddleware.js
@@ -1,4 +1,6 @@
 import { defaultValueSchemable } from "sequelize/lib/utils";
+import models, { sequelize } from "../config/db.js";
+import { patchTransactionPayload, isPayloadIdentical } from "../services/transactionService.js";
 
 export const validate = (schema) => {
     return (req, res, next) => {
@@ -35,5 +37,48 @@ export const performValidation = (schema, data) => {
         isValid: true,
         value: value,
         errors: null,
+    }
+}
+
+export const fetchUpdateableTransaction = async (req, res, next) => {
+    try {
+        const txn = await models.transactions.findByPk(req.params.transactionId, {
+            include: {
+                model: models.transaction_items,
+                as: "transaction_items",
+            }
+        });
+
+        if (!txn) {
+            return res.status(404).json({ message: "Transaction not found." });
+        }
+
+        const updatedPayload = patchTransactionPayload(txn.toJSON(), req.body);
+        isPayloadIdentical(txn.toJSON(), updatedPayload);
+
+        req.oldTxn = txn.toJSON();
+        req.updatedPayload = updatedPayload;
+
+        next();
+    }
+    catch (error) {
+        return res.status(error.status || 500).json({ message: error.message });
+    }
+}
+
+export const carvePayload = async (req, res, next) => {
+    try {
+        delete req.updatedPayload["transaction_id"];
+        for (const item of req.updatedPayload.transaction_items) {
+            delete item["transaction_id"];
+            delete item["item_id"];
+            delete item["product_name"];
+            delete item["product_unit_price"];
+        }
+
+        next();
+    }
+    catch (error) {
+        return res.status(error.status || 500).json({ message: error.message });
     }
 }
