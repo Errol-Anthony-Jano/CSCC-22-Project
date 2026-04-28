92ca4c3aac5a36f368812d76478169d5d8d66e1c
Errol Anthony Jano <200910884@my.xu.edu.ph>
2026-04-28 11:35:12 +0800

    Add mutator hooks for post/patch requests

--- frontend/src/hooks/useProducts.js
+++ frontend/src/hooks/useProducts.js
@@ -1,12 +1,34 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import api from "../api/api.js";
 
 export const useProducts = () => {
-    return useQuery({
+    const queryClient = useQueryClient();
+    const query = useQuery({
         queryKey: ['products'],
         queryFn: async () => {
             const { data } = await api.get('/products');
             return data;
         }
-    })
+    });
+
+    // The Updater (Mutation)
+    const updateMutation = useMutation({
+        mutationFn: async (updatedProduct) => {
+            return api.patch(`/products/${updatedProduct.product_id}`, updatedProduct);
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ['products'] });
+        },
+    });
+
+    const insertMutation = useMutation({
+        mutationFn: async (newProduct) => {
+            return api.post('/products', newProduct);
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ['products'] });
+        },
+    });
+
+    return { ...query, updateMutation, insertMutation };
 }
