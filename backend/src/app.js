0209813c7a445859b8d33d5a77ca6c83e4f25e1a
Errol Anthony Jano <200910884@my.xu.edu.ph>
2026-04-28 11:51:24 +0800

    Allow patch requests in application

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,8 +10,8 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: 'http://localhost:5173', // Your Vite port
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
 }))
 
 app.get('/', (req, res) => {
